28c1c1961fd633611ebab1f9ebd3435579138c42
Kevin Alford <zeroeth@pixelflow.org>
2011-10-31 16:06:34 -0400

    Initial rails app

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+gem 'pg'
+gem 'haml'
+
+group :development, :test do
+  gem 'fabrication'
+  gem 'rspec'
+  gem 'cucumber-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HighScores::Application.config.secret_token = '825601c08ad819aed153eb06bc481ca54ce256c0c3b001403196e96d537e3743c43e6deeb35540008b6a6e0168440b002ec5e96a83249e3e0df2b8a065453657'
